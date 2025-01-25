015bcd66de36e4ae45f96ce7f77f7522165630fe
DongWook <star00301@naver.com>
2025-01-25 20:53:36 +0900

    fix.Header 영역 밑줄 수정중...

--- src/headerScript.js
+++ src/headerScript.js
@@ -11,7 +11,6 @@ export const getStorage = (key = "movie") => {
 };
 
 export const saveStorage = (jsonObj, movieTitle) => {
-  console.log("jsonObj ->", jsonObj);
   if (jsonObj.Response === "False") {
     console.log("title ->", movieTitle);
     sessionStorage.setItem("inputVal", movieTitle);
@@ -20,9 +19,9 @@ export const saveStorage = (jsonObj, movieTitle) => {
   }
 };
 
-export const getDataFromApi = async (title) => {
+export const getDataFromApi = async (movieTitle) => {
   const response = await fetch(
-    `https://www.omdbapi.com/?apikey=524002e8&t=${title}`
+    `https://www.omdbapi.com/?apikey=524002e8&t=${movieTitle}`
   );
   const jsonData = await response.json();
   return jsonData;
@@ -33,7 +32,21 @@ export const headerScript = () => {
   const searchIcon = document.querySelector(".header__search-icon");
   const btnSearch = document.querySelector(".header__search-btn");
   const headerInput = document.querySelector(".header__input");
-  const inputForm = document.querySelector(".header__search");
+  const inputForm = document.querySelector(".header__search-form");
+
+  const elementsHide = () => {
+    headerInput.value = "";
+    headerInput.style.display = "none";
+    btnX.style.display = "none";
+    btnSearch.style.visibility = "hidden";
+  };
+
+  const elementsShow = () => {
+    headerInput.style.display = "block";
+    btnX.style.display = "block";
+    btnSearch.style.visibility = "visible";
+    searchIcon.style.display = "none";
+  };
 
   inputForm.addEventListener("submit", async (event) => {
     //실행순서 바꾸지 말기.
@@ -43,10 +56,7 @@ export const headerScript = () => {
     saveStorage(jsonObj, headerInput.value);
     // sessionStorage.setItem("movie", JSON.stringify(temp));
     setTimeout(() => {
-      headerInput.style.display = "none";
-      headerInput.value = "";
-      btnX.style.display = "none";
-      btnSearch.style.visibility = "hidden";
+      elementsHide();
       searchIcon.style.display = "block";
     }, 10);
     location.href = "http://localhost:5500/src/pages/result/result.html";
@@ -65,19 +75,11 @@ export const headerScript = () => {
   });
 
   headerInput.addEventListener("focus", () => {
-    headerInput.style.display = "block";
-    btnX.style.display = "block";
-    btnSearch.style.visibility = "visible";
-    searchIcon.style.display = "none";
+    elementsShow();
   });
 
   headerInput.addEventListener("focusout", () => {
-    setTimeout(() => {
-      headerInput.style.display = "none";
-      headerInput.value = "";
-      btnX.style.display = "none";
-      btnSearch.style.visibility = "hidden";
-    }, 100);
+    elementsHide();
     setTimeout(() => {
       searchIcon.style.display = "block";
     }, 300);
@@ -91,10 +93,5 @@ export const headerScript = () => {
   searchIcon.addEventListener("click", () => {
     headerInput.style.display = "block";
     headerInput.focus();
-  });
-
-  btnX.addEventListener("click", (e) => {
-    console.log("btn X!!");
-    e.stopImmediatePropagation();
   });
 };
--- src/pages/result/script.js
+++ src/pages/result/script.js
@@ -7,20 +7,22 @@ import {
   saveStorage,
 } from "../../headerScript.js";
 
-const searchBtn = document.querySelector(".search-bar__btn");
 const resultInput = document.querySelector(".search-bar__input");
 const resultCard = document.querySelector(".search-result__cards");
 const searchbarForm = document.querySelector(".search-bar-wrapper");
+const rcmdContent = document.querySelector(".recommend__content");
 
 loadHeader().then(() => {
   headerScript();
   resultPageInit();
+  fetchData();
+  console.log("loadheader.....");
 });
 
 const resultPageInit = () => {
   const headerInputVal = getStorage("inputVal");
   const headerIcon = document.querySelector(".header__search-icon");
-  headerIcon.style.visibility = "hidden";
+  // headerIcon.style.visibility = "hidden";
 
   let movie;
   if (headerInputVal) {
@@ -32,21 +34,6 @@ const resultPageInit = () => {
     resultInput.placeholder = `The result of a search for ${movie.Title}`;
   }
 };
-
-//Result 페이지 서치바 버튼 영역
-searchbarForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  console.log("here is submit......");
-  let jsonObj = await getDataFromApi(resultInput.value);
-  console.log("submit.. title ->", resultInput.value);
-  saveStorage(jsonObj, resultInput.value);
-  // sessionStorage.setItem("movie", JSON.stringify(temp));
-  location.href = "http://localhost:5500/src/pages/result/result.html";
-});
-
-resultInput.addEventListener("focus", () => {
-  resultInput.placeholder = "";
-});
 
 function resultRender(movie) {
   if (movie.Title === undefined) {
@@ -78,3 +65,53 @@ function resultRender(movie) {
     sessionStorage.removeItem("movie");
   }
 }
+
+function dataRender(data, container) {
+  console.log("DataRender.......");
+  container.innerHTML = data
+    .map(
+      (movie) => `
+              <article class="movie-card">
+                  <div class="movie-card__imgcontainer">
+                      <a href="/src/pages/detail/detail.html?id=${movie.imdbID}">
+                          <img src="${movie.Poster}" alt="${movie.Title}">
+                      </a>
+                  </div>
+                  <h2 class="movie-title">${movie.Title}</h2>
+                  <div class="post-info">
+                  <span class="movie-year">${movie.Year}</span> • 
+                  <span class="movie-runtime">${movie.Runtime}</span>
+                  </div>
+              </article>
+              `
+    )
+    .join("");
+}
+
+async function fetchData() {
+  console.log("fetchData......");
+  try {
+    const response = await fetch("/src/data/movieData.json"); // 미리 저장한 json 파일 fetch
+    const data = await response.json();
+
+    dataRender(data, rcmdContent);
+  } catch (err) {
+    // 디펜시브 코딩 (실패 시 에러메세지 도출)
+    console.error("Error!", err);
+  }
+}
+
+//Result 페이지 서치바 버튼 영역
+searchbarForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  console.log("here is submit......");
+  let jsonObj = await getDataFromApi(resultInput.value);
+  console.log("submit.. title ->", resultInput.value);
+  saveStorage(jsonObj, resultInput.value);
+  // sessionStorage.setItem("movie", JSON.stringify(temp));
+  location.href = "http://localhost:5500/src/pages/result/result.html";
+});
+
+resultInput.addEventListener("focus", () => {
+  resultInput.placeholder = "";
+});
